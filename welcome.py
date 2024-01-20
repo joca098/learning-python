1f596627da9b6b88d3a6f403bfd0b515c8e8eea0
Jcamilo <camilo_098@hotmail.com>
2024-01-20 09:12:14 -0500

    Refactoring code

--- welcome.py
+++ welcome.py
@@ -2,7 +2,6 @@ name = "Camilo"
 country= "Colombia"
 birthday = 1998
 now = 2024
-print("Hola soy",name,"y soy de",country)
 
 print("You're", name, "and you're from",country)
 print("your age is", now - birthday)
