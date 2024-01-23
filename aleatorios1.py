85f71193c8a0118f0647e632f6a8269fae354288
joca98 <camilo_098@hotmail.com>
2024-01-22 19:25:43 -0500

    Codigo documentariado

--- aleatorios1.py
+++ aleatorios1.py
@@ -1,7 +1,13 @@
 import os
 from random import randint, uniform
+
+#randint = Genera un numeros aleatorios enteros en un rango especificado por el desarrollador
+#uniform= genera numeros aleatorios flotantes o decimales 
+
 os.system('clear')
+
 n1 = randint(-100,100)
 n2 = uniform(1,50)
+
 print(n1)
 print(n2)
