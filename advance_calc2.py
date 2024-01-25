7408fc58d971b7e5b034f2cbdb61793a2f0f3e95
Jcamilo <camilo_098@hotmail.com>
2024-01-25 18:54:08 -0500

    Actualizado

--- advance_calc2.py
+++ advance_calc2.py
@@ -1,14 +1,15 @@
 '''
 Script description:
-calculadora que reciba 2 numeros y 
-realice la operacion aritmetica que el usuario desee.
+Calculadora que reciba 2 números y
+realice la operación aritmética que el usuario desee.
 Puede escoger entre sumar, restar, mul o dividir.
 '''
 import os
+
 os.system("clear")
 
-def calculator(x,y,z):
-    if z == '1' :
+def calculator(x, y, z):
+    if z == '1':
         return x + y
     elif z == '2':
         return x - y
@@ -16,26 +17,38 @@ def calculator(x,y,z):
         return x * y
     elif z == '4':
         if y == 0:
-            return 'No es posible dividir entre cero(0)'
+            return 'No es posible dividir entre cero (0)'
         else:
             return x / y
     elif z == '5':
-        return x + y , x - y, x * y, x / y 
-    else:
-        print("::: Fail, invalid option :::") 
+        if y == 0:
+            return x+y, x-y, x*y,'No es posible dividir entre cero (0)'
+        else:   
+            return x+y, x-y, x*y, x/y
+    else :
         return 'Fail, invalid option'
-    ##que haya una opcion 5 que permita ejecutar las 4 operaciones
 
-n1 =  float(input("Ingrese primer valor: "))
-n2 =  float(input("Ingrese segundo valor: "))
+n1 = float(input("Ingrese primer valor: "))
+n2 = float(input("Ingrese segundo valor: "))
 
-print("::: MENÚ CALCULADORA :::")
+print(":::: MENÚ CALCULADORA ::::")
 print("[1]. Sumar")
 print("[2]. Restar")
 print("[3]. Multiplicar")
-print("[4]. Dividir") 
-print("[5]. TODAS") 
-opc = input("Digite una opcion del menu: ")
+print("[4]. Dividir")
+print("[5]. Todas")
+opc = input("Digite una opción del menú: ")
 
-ans = calculator(n1, n2, opc)
-print("Resultado: ", ans)
+#Formato de salida 1
+if opc == '1' or opc == '2' or opc == '3' or opc == '4':
+    ans = calculator(n1, n2, opc)
+    print("Resultado: ", ans)
+elif opc == '5':
+    ans = calculator(n1, n2, opc)
+    print("Suma:", ans[0])
+    print("Resta:", ans[1])
+    print("Multiplicación:", ans[2])
+    print("Divisón:", ans[3])
+
+#Formato de salida 2
+#print("Resultado:", calculator(n1, n2, opc))
